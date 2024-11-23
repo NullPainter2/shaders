b51d9f33d8b3f9ac4151f4e133e353668f20a52e
NullPainter <nullpainter@seznam.cz>
2024-11-23 13:17:37 +0100

    ... make windows below each other - easier to compare

--- reduced-color-bits.glsl
+++ reduced-color-bits.glsl
@@ -58,11 +58,20 @@ vec3 ColorWindows( vec2 uv, vec3 col, vec3 color0, vec3 color1, vec3 color2, vec
     else if(InRect( uv, 2 ))
     {
         uv = ToRect( uv, 2 );
+
+
         col = Reduce( col, color2.r, color2.g, color2.b );
     }    
     else // 3
     {
         uv = ToRect( uv, 3 );
+
+        // color3 = color0 * color3; // this looks almost same as original color! // color^2
+
+        // color3 = color0 / color1; // dims/darkens color a bit; has bands at strange places
+        
+        // color3 = color0 - color3; // This looks neat! for rgb(8bit,8bit,8bit) - luma(16bit)
+
         col = Reduce( col, color3.r, color3.g, color3.b );
     }
     
@@ -79,29 +88,45 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     //
     // Define how many bits should each R G B have for each of 4 windows
     //
+    // Bits are drawn into windows in this order:
+    //
+    //   bits1   |  bits2
+    // -------------------
+    //   bits3   |  bits4
+    //          
 
     vec3 bits1, bits2, bits3, bits4;
 
     bits1 = vec3( 8., 8., 8. ); // R G B = 8, 8, 8
 
-    //bits2 = vec3( 7., 7., 7. ); // this has banding!
-    bits2 = vec3( 5., 6., 5. ); // R G B = 5 6 5
-
-    bits3 = vec3( 5., 7., 4. );
+    //bits3 = vec3( 7., 7., 7. ); // this has banding!
+    bits3 = vec3( 5., 6., 5. ); // R G B = 5 6 5
+    //bits3 = vec3( 2., 4., 2. );
+    //bits3 = vec3( .5, .6, .5 ); // 1.6bit :D not bad tbh
+    //bits3 = vec3( .05, .06, .05 );   // .16bit :D has black bars
 
     //
     // RGB bits based on luminance
     //
     // https://en.wikipedia.org/wiki/Grayscale
     {
-        float     BITS = 16.;
+        float     BITS = 32.; // produces banding even for 32 or 64 ... likely because the blue has smallest resolution so it jumps - 10%)
         vec3 LUMINANCE = vec3( .2126, .7152, .0722 );
+
+        bits2 = BITS * LUMINANCE;
+    }
+
+    // Let's ignore other channels to see what banding is there with just one channel ...
+    {
+        float     BITS = 32.;
+        
+        vec3 LUMINANCE = vec3( 0, 0, .0722 ); // That banding is not caused by blue being lowest resolution always!
+        // vec3 LUMINANCE = vec3( .2126, 0, 0 ); // I don't see any  big banding corellation there
 
         bits4 = BITS * LUMINANCE;
     }
-    //bits4 = vec3( 2., 4., 2. );
-    //bits4 = vec3( .5, .6, .5 ); // 1.6bit :D not bad tbh
-    //bits4 = vec3( .05, .06, .05 );   // .16bit :D has black bars
+    // bits4 = vec3( 6., 8., 2. ); /// this one is kinda decent, but with some bands
+    
 
 
     //
